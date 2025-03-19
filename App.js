f09048ba1ab32f63e2bd8f276ab852e9bf73fb0f
Jenny-Velichkova <kiscompany@gmail.com>
2025-03-19 03:19:11 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FileReader from './components/FileReader';
+
+function App() {
+  return (
+    <div>
+      <FileReader />
+    </div>
+  );
+}
+
+export default App;
